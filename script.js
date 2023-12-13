94365b770f834fdaff19d27abf496d90ff15274b
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-12-13 16:47:43 +0100

    fix bug not string in li tag

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const {createApp} = Vue
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            mail : []
+    data() {
+        return {
+            mail: [""]
+        };
+    },
+    methods: {
+        genMail() {
+            const singleEmailPromise = axios.get('https://flynn.boolean.careers/exercises/api/random/mail');
+            
+            singleEmailPromise
+                .then(response => {
+                    this.mail.push(response.data.response.email);
+
+                    if (this.mail.length < 10) {
+                        this.genMail();
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        },
+        allMails() {
+            const emailRequests = Array.from({ length: 10 }, () => axios.get('https://flynn.boolean.careers/exercises/api/random/mail'));
+
+            AttendMail.all(emailRequests)
+                .then(responses => {
+                    this.mail = responses.map(response => response.data.response.email);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
         }
     },
-    methods : {
-
-    },
-    mounted(){
-        axios.get('https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js')
-        .then(response=>{
-            this.mail.push(response.data.response.email);
-
-            if (this.mail.lenght < 10){
-                
-            }
-            
-        })
+    mounted() {
+        this.genMail();
     }
-}).mount('#app')
+}).mount('#app');
