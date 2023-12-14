2c11cb8ff43171d63fe7224e5ff12e58868f50dd
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-12-14 09:42:55 +0100

    correzione in classe

--- script.js
+++ script.js
@@ -7,34 +7,17 @@ createApp({
         };
     },
     methods: {
-        genMail() {
-            const singleEmailPromise = axios.get('https://flynn.boolean.careers/exercises/api/random/mail');
-            
-            singleEmailPromise
-                .then(response => {
-                    this.mail.push(response.data.response.email);
-
-                    if (this.mail.length < 10) {
-                        this.genMail();
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        },
-        allMails() {
-            const emailRequests = Array.from({ length: 10 }, () => axios.get('https://flynn.boolean.careers/exercises/api/random/mail'));
-
-            AttendMail.all(emailRequests)
-                .then(responses => {
-                    this.mail = responses.map(response => response.data.response.email);
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        }
     },
     mounted() {
-        this.genMail();
+       
+        for (let i=0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((stampa) =>{
+                this.mail.push(stampa.data.response)
+            })
+
+
+
+        }
     }
 }).mount('#app');
